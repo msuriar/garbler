12a0b5b2c2629fb690c18a457697bfe5afb697e4
Murali Suriar <code@suriar.com>
2014-01-08 23:52:48 +0000

    Update copyright year.

--- rip.go
+++ rip.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Murali Suriar
+ * Copyright 2014 Murali Suriar
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
